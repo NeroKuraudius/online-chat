fa8627d7698c48a9e63afbb9ee7f2ca89380937a
Nero <karta0706566@gmail.com>
2023-08-26 16:17:07 +0800

    fear: bulid connection between server & client

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const { Server } = require('http')
+const server = Server(app)
+const socketio = require('socket.io')
+const io = socketio(server)
+
+
+app.get('/',(req,res)=>{
+  return res.sendFile(__dirname+'/app.html')
+})
+
+io.on('connection',(socket)=>{
+  console.log('Hello, world!')
+
+  socket.on('Greet',()=>{
+    socket.emit('Greet','Catch you, client!')
+  })
+
+  socket.on('disconnect',()=>{
+    console.log('Good bye, world!')
+  })
+})
+
+const port = process.env.PORT || 3000
+server.listen(port,()=>{
+  console.log(`App is listening on ${port}`)
+})
