66e581843a690a33e6d4406f052e8708dd8ea3a7
Nero <karta0706566@gmail.com>
2023-08-31 14:39:47 +0800

    feat: add signup function

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const flash = require('connect-flash')
 const signinPassport = require('./config/passport.js')
 const { authenticator } = require('./auth.js')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const User = require('./model/User.js')
 
 app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'index.hbs' }))
 app.set('view engine', 'hbs')
@@ -43,6 +45,30 @@ app.post('/signout', authenticator, (req, res, next) => {
     req.flash('successMsg', '已成功登出')
     return res.redirect('/signin')
   })
+})
+
+app.post('/signup',async(req,res)=>{
+  const { account , name,password, passwordCheck} = req.body
+  if (!account.trim() || !name.trim() || !password.trim() || !passwordCheck.trim()){
+    req.flash('dangerMsg','所有欄位皆不得為空')
+    return res.redirect('/signin')
+  }
+  if (password !== passwordCheck){
+    req.flash('dangerMsg','兩次輸入密碼不一致')
+    return res.redirect('/signin')
+  }
+  try{
+    const registeredUser = await User.findOne({account})
+    if (registeredUser){
+      req.flash('dangerMsg','該Email已註冊')
+      return res.redirect('/signin')
+    }
+    await User.create({ account, name, password: bcrypt.hashSync(password,12)})
+    req.flash('successMsg','註冊成功，請以新帳號登入')
+    return res.redirect('/signin')
+  }catch(err){
+    console.log(`signup error: ${err}`)
+  }
 })
 
 app.get('/', authenticator, (req, res) => {
